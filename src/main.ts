ddb8bf2679ac905a31e5c34bd99cefad713d3ce0
TechQuery <shiy007@qq.com>
2021-01-17 03:44:55 +0800

    [revert] Vue CLI

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import Vue from 'vue';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 import App from './App.vue';
 import router from './router';
@@ -11,4 +13,4 @@ Vue.use(BootstrapVue).use(IconsPlugin);
 new Vue({
   router,
   render: h => h(App)
-}).$mount(document.body.appendChild(document.createElement('div')));
+}).$mount('#app');
