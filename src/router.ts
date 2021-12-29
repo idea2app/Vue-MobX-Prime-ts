3194769d0ff897a102997915ba089bb861b35c4a
TechQuery <shiy007@qq.com>
2021-12-30 02:30:31 +0800

    [fix] Vue Router error

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 import Home from './views/Home.vue';
 
 export default createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
