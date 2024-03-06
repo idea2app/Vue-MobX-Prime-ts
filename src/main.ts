a898c9559482dcaa64ad647277bc392a48cc31f4
TechQuery <shiy2008@gmail.com>
2024-03-06 16:54:19 +0800

    [migrate] replace Bootstrap & CoreUI with PrimeFlex & PrimeVue [optimize] update Upstream packages

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue';
 import '@coreui/coreui/dist/css/coreui.min.css';
+import PrimeVue from 'primevue/config';
 
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(PrimeVue).mount('#app');
