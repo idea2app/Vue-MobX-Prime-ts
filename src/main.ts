1a7c27368544af151ce235ca2687f488a6acaa01
TechQuery <shiy2008@gmail.com>
2024-04-12 03:38:53 +0800

    [add] PWA configuration based on Workbox 7

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
+import { auto } from 'browser-unhandled-rejection';
+import { serviceWorkerUpdate } from 'web-utility';
 import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
 import ConfirmationService from 'primevue/confirmationservice';
 
 import App from './App.vue';
 import router from './router';
+
+auto();
+
+self.addEventListener('unhandledrejection', ({ reason }) => {
+  const { message } = reason as Error;
+
+  if (message) self.alert(message);
+});
+
+const { serviceWorker } = window.navigator,
+  NODE_ENV = process.env.NODE_ENV || 'development';
+
+if (NODE_ENV !== 'development')
+  serviceWorker
+    ?.register('sw.js')
+    .then(serviceWorkerUpdate)
+    .then(worker => {
+      if (window.confirm('New version of this Web App detected, update now?'))
+        worker.postMessage({ type: 'SKIP_WAITING' });
+    });
+
+serviceWorker?.addEventListener('controllerchange', () =>
+  window.location.reload()
+);
 
 createApp(App).use(router).use(PrimeVue).use(ConfirmationService).mount('#app');
